5a8454e3a175b5f5bdff149320fd35c8fcc3df25
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-01 12:59:11 -0700

    added click within the button and update hbs

--- public/Burger.js
+++ public/Burger.js
@@ -1,3 +1,5 @@
+// //define counter to help identify each li element 
+// let burgerCounter = 0
 //axios to hit route to retrieve all groceries with button
 document.getElementById('addBurger').addEventListener('click', event => {
     event.preventDefault()
@@ -7,38 +9,17 @@ document.getElementById('addBurger').addEventListener('click', event => {
     })
     .then(({ data }) => {
         // console.log(data)
-        let burgerElem = document.createElement('li')
-            // burgerElem.classList.add('notDevoured')
-            burgerElem.innerHTML = `
-            ${document.getElementById('burgerName').value}
-            <button id="devour" data-id="${data.insertedId}">Devour</button>
-            `
-            // append element onto burger list
-            document.getElementById('burgersList').append(burgerElem)
+        location.reload()
     }) 
     .catch(err => console.error(err))
 })  
 
-document.getElementById('devour').addEventListener('click', event => {
-    if (event.target.className === 'devour') {
-    axios.put(`/api/burgers/${event.target.dataset.id}`, {devoured: 1})
+let  eatBurger = ( id => {
+      console.log('ping:', id)
+    axios.put(`/api/burgers/${id}`, {devoured: true})
       .then(({ data }) => {
-        const nomElem = document.createElement('li')
-        nomElem.innerHTML = `
-        ${event.target.value}`
-        document.getElementById('devoured').append(nomElem)
+        console.log(data)
+   location.reload()
       })
       .catch(err => console.log(err))
-    }
-})
-
-// const getBurgers = () => {
-//     axios.get('/api/burgers')
-//     .then(({data}) => {
-//         // console.log(data)
-//         data.forEach(burger => {
-            
-//         })
-//     })
-//     .catch(err => console.error(err))
-// } 
+    })
